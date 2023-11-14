69195dcc20c37aa896b78f8e5bb7aa8864745bf5
JustinT9 <jus81818@yahoo.com>
2023-11-13 23:51:33 -0500

    more progress

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,65 @@
-import api_key from "../config.js"; 
+import { api_key, city, state} from "../config.js"; 
+
+const updateInfo = (city, state, date) => {
+    const locationElement = document.querySelector('h1'); 
+    const dateElement = document.querySelector('h3'); 
+    const dateObj = new Date(date); 
+
+    locationElement.textContent = `${city}, ${state}`; 
+    dateElement.textContent = `${dateObj.getDay().toLocaleString()}, ${dateObj.getMonth().toLocaleString()} ${dateObj.getDate()}`; 
+}; 
+
+const updateTemp = (temp, maxTemp, minTemp) => {
+    const tempElement = document.querySelector('.temp h1'); 
+    const minMaxElement = document.querySelector('.temp h6'); 
+
+    tempElement.textContent = Math.round(temp); 
+    minMaxElement.textContent = `${Math.round(maxTemp)}` + ` / ` + `${Math.round(minTemp)}`; 
+}; 
 
 const getWeather = () => {
     const key = api_key; 
-    const city = "Scotch Plains"; 
-    const state = "NJ"; 
     const proxy = "https://cors-anywhere.herokuapp.com/";   
-    const location = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},US&appid=${key}`;
+    const locationEndpoint = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},US&appid=${key}`;
      
-    const response = fetch(proxy + location, {
+    const location = fetch(proxy + locationEndpoint, {
         method: 'GET', 
         headers: {
             'Content-Type': 'application/json'
         }
     }).then(res => {
         return res.json(); 
-    })
-    .then(data => console.log(data))
-    .catch(error => console.log(error)) 
+
+    }).then(data => {
+        console.log(data); 
+        const lat = data[0].lat; 
+        const lon = data[0].lon; 
+        const newCity = data[0].name; 
+        const newState = data[0].state; 
+
+        // fahrenheit
+        const metric = 'imperial'
+        console.log(lat, lon); 
+
+        const weatherEndpoint = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=${metric}`;
+
+        const weather = fetch(proxy + weatherEndpoint, {
+            method: 'GET', 
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => {
+            return res.json(); 
+
+        }).then(data => { 
+            console.log(data); 
+            updateInfo(newCity, newState, data.dt); 
+            updateTemp(data.main.temp, data.main.temp_max, data.main.temp_min); 
+
+        }).catch(error => console.log(error)) 
+
+    }).catch(error => console.log(error)) 
 }
 
-// getWeather(); 
+getWeather(); 
+
