0818f9b40df842b937abc4db161bd72b24d946b7
Justin Tran <jus81818@yahoo.com>
2023-12-10 15:04:41 -0500

    added search feature along with different pages and weather switching through event listeners

--- scripts/script.js
+++ scripts/script.js
@@ -92,7 +92,7 @@ const setTemp = (metric, temp, maxTemp, minTemp) => {
     container.appendChild(minMaxElement); 
 }; 
 
-const setStats = (windSpeed, data, humidity, cloudiness) => {  
+const setStats = (type, windSpeed, data, humidity, cloudiness) => {  
     const windElement     = document.querySelector(".windSpeed"); 
     const rainElement     = document.querySelector('.rainVolume'); 
     const humidityElement = document.querySelector(".humidity");
@@ -101,36 +101,9 @@ const setStats = (windSpeed, data, humidity, cloudiness) => {
 
     windElement.textContent     = `${Math.round(windSpeed)} mph`; 
     humidityElement.textContent = `${humidity}%`; 
-    rainElement.textContent     = (precipitation ? `${precipitation["1h"]}mm` : `0 mm`);
+    rainElement.textContent     = ((type === "daily" && precipitation) ? `${precipitation["1h"]}mm` : 
+                                   ((type === "week" && precipitation) ? `${data.rain} mm` : `0 mm`));
     cloudyElement.textContent   = `${cloudiness}%`; 
-}; 
-
-const setDaily = (dailyInfo) => {
-    const forecastElement = document.querySelector('.dailyForecast'); 
-
-    dailyInfo.forEach((info) => {
-        const hourlyForecast = document.createElement('div'); 
-        const iconElement    = document.createElement('i'); 
-        const timeElement    = document.createElement('h4'); 
-        const tempElement    = document.createElement('h4'); 
-
-        const time = new Date(info.dt * 1000); 
-        const description = info.weather[0].description; 
-
-        setIcon(description, iconElement); 
-        hourlyForecast.className = "hourlyForecast"; 
-        timeElement.textContent = (time.toLocaleTimeString().length % 2 === 0) ? 
-        (`${time.toLocaleTimeString().substring(0, 4)} 
-        ${time.toLocaleTimeString().substring(8, time.toLocaleTimeString().length)}`) : 
-        (`${time.toLocaleTimeString().substring(0, 5)} 
-        ${time.toLocaleTimeString().substring(9, time.toLocaleTimeString().length)}`); 
-        tempElement.textContent = Math.round(info.main.temp) + "\u00b0"; 
-
-        hourlyForecast.appendChild(timeElement); 
-        hourlyForecast.appendChild(iconElement); 
-        hourlyForecast.appendChild(tempElement);
-        forecastElement.appendChild(hourlyForecast); 
-    }); 
 }; 
 
 const setHighlights = (feelsLike, visibility, sunriseTime, sunsetTime) => {
@@ -162,6 +135,34 @@ const setHighlights = (feelsLike, visibility, sunriseTime, sunsetTime) => {
     sunsetElement.appendChild(sunsetInfo); 
 }; 
 
+const setDaily = (dailyInfo) => {
+    const forecastElement = document.querySelector('.dailyForecast'); 
+
+    dailyInfo.forEach((info) => {
+        const hourlyForecast = document.createElement('div'); 
+        const iconElement    = document.createElement('i'); 
+        const timeElement    = document.createElement('h4'); 
+        const tempElement    = document.createElement('h4'); 
+
+        const time        = new Date(info.dt * 1000); 
+        const description = info.weather[0].description; 
+
+        setIcon(description, iconElement); 
+        hourlyForecast.className = "hourlyForecast"; 
+        timeElement.textContent  = (time.toLocaleTimeString().length % 2 === 0) ? 
+        (`${time.toLocaleTimeString().substring(0, 4)} 
+        ${time.toLocaleTimeString().substring(8, time.toLocaleTimeString().length)}`) : 
+        (`${time.toLocaleTimeString().substring(0, 5)} 
+        ${time.toLocaleTimeString().substring(9, time.toLocaleTimeString().length)}`); 
+        tempElement.textContent = Math.round(info.main.temp) + "\u00b0"; 
+
+        hourlyForecast.appendChild(timeElement); 
+        hourlyForecast.appendChild(iconElement); 
+        hourlyForecast.appendChild(tempElement);
+        forecastElement.appendChild(hourlyForecast); 
+    }); 
+}; 
+
 // sets up the weekly forecast weather 
 const setWeekly = (weekInfo) => {
     const Days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", 
@@ -182,8 +183,7 @@ const setWeekly = (weekInfo) => {
         setIcon(description, icon); 
         (idx === 0) ? day.textContent = "Today" :  
         (day.textContent = `${Days[new Date(daily.dt * 1000).getDay()]}`);
-        temp.textContent = `${Math.round(daily.temp.max) + "\u00b0"} / 
-                            ${Math.round(daily.temp.min) + "\u00b0"}`;
+        temp.textContent = `${Math.round(daily.temp.max) + "\u00b0"} / ${Math.round(daily.temp.min) + "\u00b0"}`;
 
         container.appendChild(day);
         container.appendChild(icon);
@@ -192,20 +192,7 @@ const setWeekly = (weekInfo) => {
     }); 
 }; 
 
-const inputLocation = () => {
-    const inputElement = document.querySelector(".addLocation"); 
-
-    inputElement.addEventListener("input", (e) => {
-        e.preventDefault(); 
-
-        
-        console.log(e.target.value); 
-    });  
-
-    console.log(inputElement); 
-}; 
-
-const currentWeather = (key, proxy, lat, lon) => {
+const currentWeather = (key, proxy, lat, lon, city) => {
     const metric = 'imperial';
     const weatherEndpoint = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=${metric}&cnt=7`;
 
@@ -220,12 +207,13 @@ const currentWeather = (key, proxy, lat, lon) => {
     }).then(data => { 
         const iconElement = document.querySelector(".currentTempInfo i"); 
 
+        setInfo(city); 
         setIcon(data.weather[0].description, iconElement); 
-        iconElement.className = `${iconElement.className} weather-icon`; 
-
-        setInfo("Scotch Plains"); 
         setTemp(metric, data.main.temp, data.main.temp_max, data.main.temp_min); 
-        setStats(data.wind.speed, data, data.main.humidity, data.clouds.all); 
+        setStats("daily", data.wind.speed, data, data.main.humidity, data.clouds.all); 
+        setHighlights(data.main.feels_like, data.visibility, data.sys.sunrise, data.sys.sunset); 
+
+        iconElement.className = `${iconElement.className} weather-icon`; 
 
     }).catch(error => console.log(error)); 
 };
@@ -242,8 +230,7 @@ const dailyForecast = (key, proxy, lat, lon) => {
     }).then(res => {
         return res.json(); 
     }).then(data => {
-        const dailyInfo = data.list
-
+        const dailyInfo = data.list;
         setDaily(dailyInfo); 
         
     }).catch(error => console.log(error));  
@@ -263,14 +250,19 @@ const weeklyForecastWeather = (key, proxy, lat, lon) => {
     }).then(data => {
         const weekInfo = data.list; 
         setWeekly(weekInfo); 
+        switchWeatherDisplay(weekInfo); 
 
     }).catch(error => console.log(error)); 
 }; 
 
-const callWeatherData = () => {
+const callWeatherData = (city, state) => {
+    console.log(city, state); 
+
     const key = api_key; 
-    const proxy = "https://cors-anywhere.herokuapp.com/";   
-    const locEndpoint = `http://api.openweathermap.org/geo/1.0/direct?q=Scotch Plains, NJ,US&appid=${key}`;
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const locEndpoint = `http://api.openweathermap.org/geo/1.0/direct?q=${city}, ${state},US&appid=${key}`;
+
+    console.log(locEndpoint); 
 
     const location = fetch(proxy + locEndpoint, {
         method: 'GET', 
@@ -282,31 +274,138 @@ const callWeatherData = () => {
     }).then(data => {
         const lat = data[0].lat; 
         const lon = data[0].lon; 
+
+        console.log(lat, lon); 
     
-        currentWeather(key, proxy, lat, lon); 
+        currentWeather(key, proxy, lat, lon, city); 
         dailyForecast(key, proxy, lat, lon); 
         weeklyForecastWeather(key, proxy, lat, lon); 
 
     }).catch(error => console.log(error)); 
 }; 
 
+const searchLocation = () => {
+    const formElement = document.querySelector('.inputWrapper'); 
+
+    formElement.addEventListener('submit', (e) => {
+        e.preventDefault(); 
+
+        clear(); 
+
+        const locationValue = document.querySelector(".addLocation").value; 
+        const pair = {"city": locationValue.split(",")[0], "state": locationValue.split(",")[1].trim()}; 
+
+        callWeatherData(pair["city"], pair["state"]); 
+    }); 
+}; 
+
+const clear = () => {
+    const geoElement     = document.querySelector(".currentGeo"); 
+    const iconElement    = document.querySelector(".currentTempInfo i"); 
+    const tempElement    = document.querySelector(".tempNum"); 
+    const climateElement = document.querySelector(".climate-stats"); 
+    const feelsElement   = document.querySelector(".feelslike"); 
+    const visibleElement = document.querySelector(".visible"); 
+    const sunriseElement = document.querySelector(".sunrise"); 
+    const sunsetElement  = document.querySelector(".sunset"); 
+
+    const dailyForecast  = document.querySelector(".dailyForecast"); 
+    const weeklyForecast = document.querySelector(".forecastWeather"); 
+
+    while (geoElement.firstChild) {
+        geoElement.firstChild.remove(); 
+    }
+
+    while (tempElement.firstChild) {
+        tempElement.firstChild.remove(); 
+    }
+
+    while (dailyForecast.firstChild) {
+        dailyForecast.firstChild.remove(); 
+    }
+
+    while (weeklyForecast.firstChild) {
+        weeklyForecast.firstChild.remove(); 
+    }
+
+    climateElement.childNodes.forEach((climate) => {
+        climate.textContent = ""; 
+    })
+
+    feelsElement.childNodes.forEach((ele) => {
+        if (ele.tagName === "H4") ele.remove();  
+    })
+
+    visibleElement.childNodes.forEach((ele) => {
+        if (ele.tagName === "H4") ele.remove(); 
+    })
+    
+    sunriseElement.childNodes.forEach((ele) => {
+        if (ele.tagName === "H4") ele.remove(); 
+    })
+
+    sunsetElement.childNodes.forEach((ele) => {
+        if (ele.tagName === "H4") ele.remove(); 
+    })
+
+    iconElement.className = ""; 
+}; 
+
+const switchWeatherDisplay = (data) => {
+    const forecastElement = document.querySelector(".forecastWeather"); 
+
+    forecastElement.addEventListener('click', () => {
+        forecastElement.childNodes.forEach((forecast, i) => {
+            forecast.addEventListener('click', () => {
+                const Days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", 
+                "Saturday"];
+                const dayElement        = document.querySelector(".currentGeo h3"); 
+                const iconElement       = document.querySelector(".currentTempInfo i");  
+                const tempElement       = document.querySelector(".tempNum h1"); 
+                const minMaxElement     = document.querySelector(".tempNum h6");  
+                
+                const max = forecast.childNodes[2].textContent.split("/")[0];
+                const min = forecast.childNodes[2].textContent.split("/")[1];  
+
+                if (forecast.childNodes[0].textContent === "Today") {
+                    const date             = new Date(Date.now());                                                                                     
+                    
+                    dayElement.textContent = (date.toLocaleTimeString().length % 2 === 0) ? 
+                    (`${Days[date.getDay()]} ${date.toLocaleTimeString().substring(0, 4)} 
+                    ${date.toLocaleTimeString().substring(8, date.toLocaleTimeString().length)}`) : 
+                    (`${Days[date.getDay()]} ${date.toLocaleTimeString().substring(0, 5)} 
+                    ${date.toLocaleTimeString().substring(9, date.toLocaleTimeString().length)}`);
+
+                } else {
+                    dayElement.textContent  = forecast.childNodes[0].textContent; 
+                    tempElement.textContent = max.trim();
+                    setStats("week", data[i].speed, data[i], data[i].humidity, data[i].clouds); 
+                }
+
+                iconElement.className     = `${forecast.childNodes[1].className} weather-icon`;   
+                iconElement.style.color   = forecast.childNodes[1].style.color; 
+                minMaxElement.textContent = `${max}/${min}`; 
+
+            }); 
+        }); 
+    }); 
+}; 
 
 const mock = () => {
     const iconElement = document.querySelector(".currentTempInfo i"); 
-    setIcon(todayWeather.weather[0].description, iconElement); 
-    iconElement.className = `${iconElement.className} weather-icon`; 
 
+    setIcon(todayWeather.weather[0].description, iconElement); 
     setInfo(todayWeather.name); 
     setTemp("imperial", todayWeather.main.temp, todayWeather.main.temp_max, todayWeather.main.temp_min); 
-    setStats(todayWeather.wind.speed, todayWeather, todayWeather.main.humidity, todayWeather.clouds.all); 
+    setStats("daily", todayWeather.wind.speed, todayWeather, todayWeather.main.humidity, todayWeather.clouds.all); 
     setDaily(todayForecast); 
     setWeekly(weekForecast);
     setHighlights(todayWeather.main.feels_like, todayWeather.visibility, todayWeather.sys.sunrise, todayWeather.sys.sunset); 
-
-    inputLocation(); 
+    iconElement.className = `${iconElement.className} weather-icon`;
+    // switchWeatherDisplay(); 
 }; 
 
 mock(); 
+searchLocation(); 
 
-// callWeatherData(); 
 
